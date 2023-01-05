fe0c9a3989cdf845610b285e71dd31e04bc05b48
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-01-05 14:21:04 +0200

    Jackson time stuff

--- src/main/java/com/griddynamics/module/ConfigModule.java
+++ src/main/java/com/griddynamics/module/ConfigModule.java
@@ -1,20 +1,28 @@
 package com.griddynamics.module;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import dagger.Module;
 import dagger.Provides;
 
 import javax.inject.Singleton;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Module
 public class ConfigModule {
     @Provides
     @Singleton
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        LocalDateTimeDeserializer localDateTimeDeserializer = new
+                LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        javaTimeModule.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
+        return new ObjectMapper()
+                .registerModule(javaTimeModule)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
 }
